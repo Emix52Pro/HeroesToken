53c62df3e1395e81fa38d4fc042d4d269f9a52f2
Gabriel <ggarcia7@utmachala.edu.ec>
2023-07-16 15:16:25 -0500

    Buscador cambiado a Heroes

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -9,13 +9,17 @@ import { Router, NavigationEnd } from '@angular/router';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnDestroy {
+  heroes: Heroe[] = [];
   heroes2: Heroe[] = [];
   private heroesSubscription: Subscription;
 
   constructor(
+    
     private _heroesService: HeroesService,
     private router: Router
   ) {
+    this.heroes = this._heroesService.getHeroes();
+    this.heroes2 = this.heroes;
     // Suscribirse a los cambios en el arreglo heroes2 del servicio
     this.heroesSubscription = this._heroesService.getHeroes2Observable().subscribe(
       (heroes) => {
@@ -30,6 +34,9 @@ export class HeroesComponent implements OnDestroy {
       }
     });
   }
+
+  public search_string: string = '';
+
   ngOnInit(): void {
 
     this.heroes2 = this._heroesService.getHeroes2();
@@ -47,5 +54,27 @@ export class HeroesComponent implements OnDestroy {
   verHeroe(idx: number) {
     console.log(idx);
     this.router.navigate(['/heroe', idx]);
+  }
+
+  updateSearchString(text: string) {
+    this.search_string = text; // Actualizar el valor local de search_string
+    this.filterResults();
+  }
+
+  filterResults() {
+    if (!this.search_string) {
+      this.heroes2 = this.heroes;
+    } else {
+      this.heroes2 = this.heroes.filter(
+        (heroe) =>
+          heroe?.nombre.toLowerCase().includes(
+            this.search_string.toLowerCase()
+          )
+      );
+    }
+
+    // Actualizar la variable en el servicio
+    this._heroesService.updateHeroes2(this.heroes2);
+    this.router.navigate(['/heroes']);
   }
 }
